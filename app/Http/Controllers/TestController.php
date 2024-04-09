aad8d9a62751a49d12d0cc4f488d8801c6c18db8
Harry Lu <harry.nan.825@gmail.com>
2024-04-09 15:20:22 +0800

    scribe x api doc x example

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,21 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TestIndexRequest;
-use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
+/**
+ * @group Test
+ * 
+ * APIs for test.
+ */
 class TestController extends Controller
 {
     /**
-     * Index for test
+     * Test index
+     * 
+     * @queryParam name string required Name description. Example: Jason
+     * @queryParam age int required Age description. Example: 17
+     * 
+     * @response 204
+     * @response scenario=success {
+     *  "id": 4,
+     *  "name": "Jessica Jones"
+     * }
+     * @response status=404 scenario="user not found" {"message": "User not found"}
      */
     public function index(TestIndexRequest $request): JsonResponse
     {
         return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA',
+            'name' => 'Abigail in controller',
+            'state' => 'CA in controller',
         ]);
     }
 }
--- app/Http/Requests/TestIndexRequest.php
+++ app/Http/Requests/TestIndexRequest.php
@@ -3,18 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Log;
 
 class TestIndexRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
